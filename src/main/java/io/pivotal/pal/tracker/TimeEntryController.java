6fdf4c53992f686ec0f77940eed2102356ba2f8d
ahmedsaeed-dev <September12!!>
2021-03-03 19:19:13 +0000

    push timeentrycontroller

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,51 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping
+
+public class TimeEntryController {
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+    @PostMapping("/time-entries")
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        return ResponseEntity.created(null).body(timeEntry);
+    }
+
+    @GetMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+        if(timeEntry == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(timeEntry);
+    }
+
+    @GetMapping("/time-entries")
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> list = timeEntryRepository.list();
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long timeEntryId, @RequestBody TimeEntry timeEntryToUpdate) {
+        TimeEntry timeEntry = timeEntryRepository.update(timeEntryId, timeEntryToUpdate);
+        if (timeEntry == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(timeEntry);
+
+    }
+
+    @DeleteMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<Void> delete(@PathVariable long timeEntryId) {
+        timeEntryRepository.delete(timeEntryId);
+        return ResponseEntity.noContent().build();
+    }
+}
